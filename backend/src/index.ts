00229a0bd15f9cecad16280cbf136b76a83a18e4
Ankur singh <13ankursingh2005@gmail.com>
2025-12-28 12:46:28 +0530

    correction

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,9 +10,9 @@ import { JWT_SECRET } from './config.js';
 import { userauth } from './middleware.js';
 import { generateHash } from './utils.js';
 import cors from 'cors';
-
-connectDB()
 dotenv.config();
+connectDB()
+
 app.use(cors());
 app.use(express.json());
 
@@ -188,8 +188,10 @@ return res.json({
 })
 })
 
-app.listen(3000,()=>{
-    console.log("Server started on port 3000");
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => {
+        console.log("Local server started on port 3000");
+    });
+}
 
 export default app;
